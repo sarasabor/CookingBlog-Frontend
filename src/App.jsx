0faf2b97c03f1a87645dd3ea38bf0a88b831b032
Sara <saborsara.wad@gmail.com>
2025-05-10 02:20:36 +0100

    start working in frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center mt-10 text-3xl text-green-600">
+      <h1>مرحبا بك فـ cookingBlog 👩‍🍳</h1>
+    </div>
+  );
+}
+
+export default App
