9fb48533d513a9e3b8022f5a0ad0286d4bb11649
Sara <saborsara.wad@gmail.com>
2025-07-15 23:00:01 +0100

    fixing bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layouts/layout";
+import Layout from "./Layouts/Layout";
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import RecipeDetails from "./pages/RecipeDetails";
